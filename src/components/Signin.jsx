5daec332d9705d6bd1f96565e0b07e0d48873b95
Avani Patel <patelavani8986@gmail.com>
2022-05-05 12:43:37 +0530

    updated signin and signup class component to function component

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,29 +1,26 @@
-import React from "react";
-class SignIn extends React.Component {
-  render() {
-    return <div className="form-body">
-      <div className="row">
-        <div className="form-holder">
-          <div className="form-content">
-            <div className="form-items">
-              <h3>Maxine</h3>
-              <div className="page-links">
-                <a href="/signin" className="active">SignIn</a><a href="/signup">Signup</a>
-              </div>
-              <form>
-                <input className="form-control" type="text" name="username" placeholder="E-mail Address" required />
-                <input className="form-control" type="password" name="password" placeholder="Password" required />
-                <div className="form-button">
-                  <button id="submit" type="submit" className="ibtn">Login</button>
-                  <a href="/forget">Forget password?</a>
-                </div>
-              </form>
+function SignIn() {
+  return <div className="form-body">
+    <div className="row">
+      <div className="form-holder">
+        <div className="form-content">
+          <div className="form-items">
+            <h3>Maxine</h3>
+            <div className="page-links">
+              <a href="/signin" className="active">SignIn</a><a href="/signup">Signup</a>
             </div>
+            <form>
+              <input className="form-control" type="text" name="username" placeholder="E-mail Address" required />
+              <input className="form-control" type="password" name="password" placeholder="Password" required />
+              <div className="form-button">
+                <button id="submit" type="submit" className="ibtn">Login</button>
+                <a href="/forget">Forget password?</a>
+              </div>
+            </form>
           </div>
         </div>
       </div>
     </div>
-  }
+  </div>
 }
 
 export default SignIn;
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,29 +1,25 @@
-import React from "react";
-class Signup extends React.Component {
-  render() {
-    return <div className="form-body">
-      <div className="row">
-        <div className="form-holder">
-          <div className="form-content">
-            <div className="form-items">
-              <h3>Maxine</h3>
-              <div className="page-links">
-                <a href="/signin">SignIn</a><a href="/signup" className="active">Signup</a>
-              </div>
-              <form>
-                <input className="form-control" type="text" name="name" placeholder="Full Name" required />
-                <input className="form-control" type="email" name="email" placeholder="E-mail Address" required />
-                <input className="form-control" type="password" name="password" placeholder="Password" required />
-                <div className="form-button">
-                  <button id="submit" type="submit" className="ibtn">Register</button>
-                </div>
-              </form>
+function Signup() {
+  return <div className="form-body">
+    <div className="row">
+      <div className="form-holder">
+        <div className="form-content">
+          <div className="form-items">
+            <h3>Maxine</h3>
+            <div className="page-links">
+              <a href="/signin">SignIn</a><a href="/signup" className="active">Signup</a>
             </div>
+            <form>
+              <input className="form-control" type="text" name="name" placeholder="Full Name" required />
+              <input className="form-control" type="email" name="email" placeholder="E-mail Address" required />
+              <input className="form-control" type="password" name="password" placeholder="Password" required />
+              <div className="form-button">
+                <button id="submit" type="submit" className="ibtn">Register</button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
     </div>
-  }
+  </div>
 }
-
 export default Signup;
